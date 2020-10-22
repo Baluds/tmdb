11fb539d3d610557f5cda1021cdd29a79ca20dea
Baluds <baluds1999@gmail.com>
2020-10-22 18:14:34 +0530

    Trailer horizontal list added

--- lib/widgets/trailer.dart
+++ lib/widgets/trailer.dart
@@ -36,52 +36,77 @@ class TrailerSection extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset(
-                        'assets/img3.jpg',
-                        fit: BoxFit.fill,
-                        width: 250,
-                        height: 160,
-                      ),
-                    ),
-                    Icon(
-                      Icons.play_arrow,
-                      size: 80,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    'Leo',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                Text(
-                  '[pop,rock,jazz]',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+            Container(
+              height: 230,
+              width: 280,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Trailerlist(),
+                  Trailerlist(),
+                  Trailerlist(),
+                ],
+              ),
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class Trailerlist extends StatelessWidget {
+  const Trailerlist({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/img3.jpg',
+                  fit: BoxFit.fill,
+                  width: 250,
+                  height: 160,
+                ),
+              ),
+              Icon(
+                Icons.play_arrow,
+                size: 80,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              'Leo',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Text(
+            '[pop,rock,jazz]',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
